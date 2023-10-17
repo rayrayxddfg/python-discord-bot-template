9ffeedfbfd77ed24c297081677d9edba700eac6a
Quantum MATRIX <143729288+quantum-foss@users.noreply.github.com>
2023-10-17 15:25:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-# main.py
 import os
-
 import discord
-from dotenv import load_dotenv
+from discord.ext import commands
 from keep_alive import keep_alive
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()  # Create a default intent
+intents.typing = False
+intents.presences = False
 
-client = discord.Client()
+# Create a bot instance with specified intents
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    print(f'{bot.user} has connected to Discord!')
 
-keep_alive()
-client.run(TOKEN)
+
+@bot.command(name='ping', help='Check the bot\'s ping time.')
+async def ping(ctx):
+    latency = bot.latency  # Get the bot's latency
+
+    # Calculate the ping time in milliseconds
+    ping_time = round(latency * 1000)
+
+    await ctx.send(f'Pong! Bot latency is {ping_time}ms')
+
+# Use an environment variable for your bot token
+TOKEN = os.environ.get('DT')
+
+if TOKEN is not None:
+    keep_alive()
+    bot.run(TOKEN)
+else:
+    print("DISCORD_TOKEN environment variable not set. Please set it with your bot token.")
